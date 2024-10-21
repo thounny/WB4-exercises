e42fb5eac564064fd3493f703fb8f26300115aa0
Thounny <thounnykeo@gmail.com>
2024-10-21 11:53:15 -0400

    For and While loops

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,4 @@
+// Define a script using a for loop to print "I love loops" 7 times
+for (let i = 0; i < 7; i++) {
+  console.log("I love loops");
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,7 @@
+// Define a script using a while loop to print "I love loops" 7 times
+let count = 0; // Initialize a counter
+
+while (count < 7) {
+  console.log("I love loops"); // Print the phrase
+  count++; // Increment the counter after each iteration
+}
